687b7b1a6b6cc99eceb1824b196e56f27db327fd
Belyashhh <61246906+Belyashhh@users.noreply.github.com>
2023-04-04 19:44:16 +0300

    проверка и сортировка

--- parserJSON.py
+++ parserJSON.py
@@ -1,74 +1,69 @@
 import json
+import TimeInterval as TI
 
 
 def readJSON(FileName="data.json"):
     with open(FileName, "r", encoding='utf-8') as f:
-        return json.load(f) 
+        return json.load(f)
 
 
 def writeJSON(data, FileName="data.json"):
     with open(FileName, "w", encoding='utf-8') as f:
-        return json.dump(data, f)      
+        return json.dump(data, f)
 
 
 def delTask(dict_data, key):
-        del dict_data[key]
-        writeJSON(data=dict_data)
+    del dict_data[key]
+    writeJSON(data=dict_data)
 
 
-def addTask(dict_data, key, start,end,name,text):
-        task = {key:{
-                "start":start,
-                "end":end,
-                "name":name,
-                "text":text}}
-        dict_data.update(task)
-        writeJSON(data=dict_data)
-        
-        
-def updateTask(dict_data, key, start,end,name,text):
-        del dict_data[key]
-        addTask(dict_data, key, start,end,name,text)
-        writeJSON(data=dict_data)
+def addTask(dict_data, key, start, end, name, text):
+    task = {key: {
+        "start": start,
+        "end": end,
+        "name": name,
+        "text": text}}
+    dict_data.update(task)
+    writeJSON(data=dict_data)
+
+
+def updateTask(dict_data, key, start, end, name, text):
+    del dict_data[key]
+    addTask(dict_data, key, start, end, name, text)
+    writeJSON(data=dict_data)
 
 
 def printTask(dict_data, key):
+    d = dict_data[key]
+    print(d["start"])
+    print(d["end"])
+    print(d["name"])
+    print(d["text"])
+    print()
 
-        d = dict_data[key]
-        print(d["start"],d["end"])
-        print(d["name"])
-        print(d["text"])
-        print()
 
 def printTaskALL(dict_data):
-        keys = dict_data.keys()
-        for key in keys:
-            printTask(dict_data, key)
-        
+    keys = dict_data.keys()
+    # print(keys)
+    skeys = TI.SortTime(keys)
+    for key in skeys:
+        printTask(dict_data, key)
+
+
+# Если есть пересечение интервалов времени - True
+# Если пересечение отсутствует - FALSE
+def checkInterval(dict_data, TimeStartNew, TimeEndNew):
+    keys = dict_data.keys()
+    for key in keys:
+        d = dict_data[key]
+        if TI.TimeInterval(d["start"], d["end"], TimeStartNew, TimeEndNew):
+            return True
+    return False
+
+
+
 
 
 
 if __name__ == '__main__':
-
-    fileName = "data.json"
-    fileName2 = "data2.json"
-    fileName3 = "data3.json"
-    #data = json.loads(get_File_Data(fileName)) 
-    
-    data = readJSON(fileName3)
-    print(data) 
-    
-    addTask(data, "2022", "2022","2023","task3","text3")
-    
     print()
-    printTask(data, key="2002") 
-    delTask(data, key="2002")
-    print()
-    
-    
-    #print(get_File_Data("data.json"))   
-    writeJSON(data, fileName3)
-    print(type(data))
-    print(data)
-    printTaskALL(data)
-    
